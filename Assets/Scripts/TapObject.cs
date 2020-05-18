1d1b29802e9ad826eb51bea03cadd4a53785e6cb
ALEXANDER MITCHELL <am186150@falmouth.ac.uk>
2020-05-18 11:30:23 +0100

    Adding the very basic commit of the project, should include a scene with a 360 image, with rotation controls. Tapping currently doesn't work in this version of the project.

---
+++ Assets/Scripts/TapObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapObject : MonoBehaviour
+{
+    public Material changeMaterial;
+    public void activate()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = changeMaterial;
+    }
+}
