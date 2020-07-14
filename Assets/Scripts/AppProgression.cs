9b14120ba8e03f77532bce8a42b5a5e0a4c8a78d
ALEXANDER MITCHELL <am186150@falmouth.ac.uk>
2020-07-14 13:28:45 +0100

    Some changes so that progress can be stored in the app.

--- Assets/Scripts/AppProgression.cs
+++ Assets/Scripts/AppProgression.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AppProgression : MonoBehaviour
 {
     private bool created = false;
+    public bool openingwatched = false;
     public bool[] levelCompleted = new bool[12];
+
+    public DisplayProgress display;
 
     // Prevent this object from being destroyed between scenes loading.
     void Awake()
@@ -14,6 +20,10 @@ public class AppProgression : MonoBehaviour
         {
             DontDestroyOnLoad(this.gameObject);
             created = true;
+            if (!LoadGame())
+            {
+                SaveGame();
+            }
         }
         else
         {
@@ -24,6 +34,62 @@ public class AppProgression : MonoBehaviour
     public void UpdateLevelCompleted(int levelIndex)
     {
         levelCompleted[levelIndex] = true;
+        SaveGame();
+    }
+
+    private Save CreateSaveGameObject()
+    {
+        Save save = new Save();
+        foreach (bool CompletedLevel in levelCompleted)
+        {
+            save.completedSections.Add(CompletedLevel);
+        }
+
+        save.openingWatched = openingwatched;
+
+        return save;
+    }
+
+    public void SaveGame()
+    {
+        Save save = CreateSaveGameObject();
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+        Debug.Log("Game Saved");
+    }
+
+    public bool LoadGame()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            Save save = (Save)bf.Deserialize(file);
+            file.Close();
+
+            for (int i = 0; i < save.completedSections.Count; i++)
+            {
+                levelCompleted[i] = save.completedSections[i];
+            }
+
+            openingwatched = save.openingWatched;
+
+            Debug.Log("Game Loaded");
+
+            display.Display();
+
+            return true;
+        }
+
+        else
+        {
+            Debug.Log("No game saved!");
+            return false;
+        }
     }
 
 }
---
+++ Assets/Scripts/DisplayProgress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayProgress : MonoBehaviour
+{
+
+    public AppProgression progress;
+    public void Display()
+    {
+        int i = 0;
+        foreach (bool level in progress.levelCompleted)
+        {
+            gameObject.GetComponent<Text>().text += "Level " + i  + " Completed " + level + "\n";
+            i++;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            progress.levelCompleted[0] = true;
+            progress.SaveGame();
+        }
+    }
+}
