0e8bfc03b309972d9dc9716525e8879db11c52da
Tim <timphillipsgames@gmail.com>
2020-08-24 16:31:29 +0100

    Working on Bee puzzle

---
+++ Assets/Images/PuzzleParts/bee/BeeBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeBehaviour : MonoBehaviour
+{
+    public bool leftFacing;
+    public float speed = 1.5f;
+    public int state = 1; //0 flying, 1 going to flower, 2 leaving.
+    public float startYpos = 0;
+    float rand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rand = Random.Range(1f, 10f); //This random value get's added to the sine wave so each bee is unique
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case 0:
+                if (leftFacing)
+                {
+                    float xpos = transform.localPosition.x - (speed * Time.deltaTime);
+                    float ypos = Mathf.Sin(Time.time + Time.deltaTime + rand) / 10;
+                    ypos += startYpos;
+                    transform.localPosition = new Vector3(xpos, ypos, transform.localPosition.z);
+                }
+                if (!leftFacing)
+                {
+                    float xpos = transform.localPosition.x + (speed * Time.deltaTime);
+                    float ypos = Mathf.Sin(Time.time + Time.deltaTime + rand) / 10;
+                    ypos += startYpos;
+                    transform.localPosition = new Vector3(xpos, ypos, transform.localPosition.z);
+                }
+                break;
+            case 1:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+
+        
+    }
+}
---
+++ Assets/Images/PuzzleParts/bee/BeeGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeGenerator : MonoBehaviour
+{
+
+    public GameObject bee;
+    public float timer = 3;
+    public bool left;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void GenerateBee()
+    {
+        if (bee != null)
+        {
+            GameObject b = Instantiate(bee, bee.transform.localPosition, bee.transform.localRotation);
+            b.transform.parent = transform;
+            b.transform.localPosition = Vector3.zero;
+            BeeBehaviour beeBahaviour = b.GetComponent<BeeBehaviour>();
+
+            float xpos = 2;
+            if (left)
+            {
+                xpos = -2;
+                beeBahaviour.leftFacing = false;
+                b.transform.localRotation = Quaternion.Euler(0, 180,0);
+            }
+            b.transform.localPosition = new Vector3(xpos, Random.Range(-0.5f, 0.5f), b.transform.localPosition.z);
+
+
+
+            //b.GetComponent<BeeBehaviour>().Initialise();
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0)
+        {
+            GenerateBee();
+            timer = 3;
+        }
+    }
+}
