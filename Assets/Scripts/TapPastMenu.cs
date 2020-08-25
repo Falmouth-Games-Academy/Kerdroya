d572993ab9073bde72bc2d212c62617ffe9957b1
JohnSpeakman <john.andrew.speakman@falmouth.ac.uk>
2020-08-25 20:31:29 +0100

    Scene re-order
    Changed default scene to main menu, updated scene ID to match scene seleciton buttons.

--- Assets/Scripts/TapPastMenu.cs
+++ Assets/Scripts/TapPastMenu.cs
@@ -17,7 +17,6 @@ public class TapPastMenu : MonoBehaviour
 
     public void ChangeScene(int input)
     {
-        Debug.Log("ASS");
         UnityEngine.SceneManagement.SceneManager.LoadScene(input);
     }
 }
