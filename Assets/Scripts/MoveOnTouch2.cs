c393379ad0ddcf440408acdf736754411860d81d
Tim <timphillipsgames@gmail.com>
2020-08-28 17:17:30 +0100

    Icons moving on touch was broken - is now fixed. Thistledown moves on touch.

---
+++ Assets/Scripts/MoveOnTouch2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnTouch2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+                transform.position = Vector3.Lerp(transform.position, touchedPos, 1);
+            }
+        }
+    }
+}
