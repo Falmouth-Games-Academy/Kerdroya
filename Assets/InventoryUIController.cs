4b7a4a00091730641916b6a138b080d31d479716
JohnSpeakman <john.andrew.speakman@falmouth.ac.uk>
2020-09-02 23:23:30 +0100

    Added fade-in to Inventory
    On completing a scene, inventory scene will now fade in

--- Assets/InventoryUIController.cs
+++ Assets/InventoryUIController.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class InventoryUIController : MonoBehaviour
 {
     public GameObject StainedWindowPanel;
+    public GameObject fadeInObject;
 
-    private float alphaDelta = 0;
+    private float alphaDelta = 1;
     public float alphaTransitionSpeed = 0.05f;
 
     private Transform[] children;
@@ -15,24 +16,7 @@ public class InventoryUIController : MonoBehaviour
 
     void Start()
     {
-        children = StainedWindowPanel.GetComponentsInChildren<Transform>();
-
-        if (AppProgression.currentComplete != -1)
-        { //render most recently unlocked frame first
-            Debug.Log("CurrentComplete = " + AppProgression.currentComplete);
-
-            for (int count = 0; count < AppProgression.levelCompleted.Length+1; count++)
-            {//remove background image and all children
-                children[count + 1].gameObject.SetActive(false);
-            }
-
-            children[AppProgression.currentComplete + 2].gameObject.SetActive(true);
-            sceneState = 0;
-        }
-        else
-        {
-            renderWindowPieces();
-        }
+        startProcedure();
     }
 
     private void Update()
@@ -40,8 +24,21 @@ public class InventoryUIController : MonoBehaviour
 
         switch (sceneState)
         {
-            case 0: FadeInPiece(); break;
-            case 1: FadeInWindow(); break;
+            case 0: FadeInWhiteout(); break;
+            case 1: FadeInPiece(); break;
+            case 2: FadeInWindow(); break;
+        }
+    }
+
+    private void FadeInWhiteout()
+    {
+        alphaDelta -= alphaTransitionSpeed;
+        fadeInObject.GetComponent<UnityEngine.UI.RawImage>().color = new Color(1, 1, 1, Mathf.SmoothStep(1.0f, 0.0f, 1-alphaDelta));
+        if (alphaDelta < 0.05f)
+        {
+            alphaDelta = 0;
+            fadeInObject.SetActive(false);
+            sceneState = 1;
         }
     }
 
@@ -53,7 +50,7 @@ public class InventoryUIController : MonoBehaviour
         {
 
             children[AppProgression.currentComplete + 2].gameObject.GetComponent<UnityEngine.UI.Image>().color = new Color(1, 1, 1, 1);
-            sceneState = 1;
+            sceneState = 2;
             renderWindowPieces();
             setWindowAlphaWithException(0, AppProgression.currentComplete);
             alphaDelta = 0;
@@ -62,7 +59,6 @@ public class InventoryUIController : MonoBehaviour
 
     private void FadeInWindow()
     {
-        Debug.Log("AYYOO");
         alphaDelta += alphaTransitionSpeed;
         setWindowAlphaWithException(alphaDelta, AppProgression.currentComplete);
         if (alphaDelta > 0.95f)
@@ -74,6 +70,7 @@ public class InventoryUIController : MonoBehaviour
 
     void renderWindowPieces()
     {
+        //set background window
         children[1].gameObject.SetActive(true);
         for (int count = 0; count < AppProgression.levelCompleted.Length; count++)
         {
@@ -84,6 +81,7 @@ public class InventoryUIController : MonoBehaviour
 
     void setWindowAlphaWithException(float alpha, int exception)
     {
+        //set background window
         children[1].gameObject.GetComponent<UnityEngine.UI.Image>().color = new Color(1, 1, 1, alpha);
         for (int count = 0; count < AppProgression.levelCompleted.Length; count++)
         {
@@ -92,6 +90,29 @@ public class InventoryUIController : MonoBehaviour
                 //count+2 used to account for background image and parent's own transform
                 children[count + 2].gameObject.GetComponent<UnityEngine.UI.Image>().color = new Color(1, 1, 1, alpha);
             }
+        }
+    }
+
+    void startProcedure()
+    {
+        children = StainedWindowPanel.GetComponentsInChildren<Transform>();
+        if (AppProgression.currentComplete != -1)
+        { //render most recently unlocked frame first
+            for (int count = 0; count < AppProgression.levelCompleted.Length + 1; count++)
+            {//remove background image and all children
+                children[count + 1].gameObject.SetActive(false);
+            }
+
+            children[AppProgression.currentComplete + 2].gameObject.SetActive(true);
+            children[AppProgression.currentComplete + 2].gameObject.GetComponent<UnityEngine.UI.Image>().color = new Color(1, 1, 1, 0);
+            //make pane invisible at startup
+            children[1].gameObject.GetComponent<UnityEngine.UI.Image>().color = new Color(1, 1, 1, 0);
+            sceneState = 0;
+        }
+        else
+        {
+            fadeInObject.SetActive(false);
+            renderWindowPieces();
         }
     }
 }
