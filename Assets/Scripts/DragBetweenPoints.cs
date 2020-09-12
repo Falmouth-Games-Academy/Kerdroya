bcb6e880ac4e47d25d8ca4b678350cbddf576361
JohnSpeakman <john.andrew.speakman@falmouth.ac.uk>
2020-09-12 03:06:26 +0100

    Predannak prototype
    Predennak minigame prototype,
    uses raycast against planes to move between collection and deposite modes, collect points by moving fingerTracker gameobject from top left to bottom right.

---
+++ Assets/Scripts/DragBetweenPoints.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragBetweenPoints : MonoBehaviour
+{
+    public GameObject targetObject;
+    public LayerMask hitLayers;
+    public MinigameProgressTracker MGPT;
+    int state = 0;
+    public SpriteRenderer SandRenderer;
+
+    public void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+        {
+            if (Vector3.Distance(targetObject.transform.position, hit.point) < 1.5f)
+            {
+                targetObject.transform.position = Vector3.Lerp(targetObject.transform.position, hit.point, 0.5f);
+            }
+
+            if(hit.transform.gameObject.name == "SAND_SOURCE")
+            {
+                if (!SandRenderer.enabled)
+                {
+                    Debug.Log("SOURCE");
+                    state = 1;
+                    SandRenderer.enabled = true;
+                }
+            }
+            if (hit.transform.gameObject.name == "SAND_GOAL")
+            {
+                if (SandRenderer.enabled)
+                {
+                    SandRenderer.enabled = false;
+                    MGPT.points++;
+                }
+            }
+        }
+    }
+}
