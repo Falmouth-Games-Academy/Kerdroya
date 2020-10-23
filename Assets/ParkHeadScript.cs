b1463fd98914fcd4792c0c5610b2667a9b00fc17
John-Speakman <45663006+John-Speakman@users.noreply.github.com>
2020-10-23 02:09:41 +0100

    Park head minigame
    Added park head minigame
    Changed "black" background material
    Repositioned  Colliford lake blackout object (avoid collission with item hunt sphere)

---
+++ Assets/ParkHeadScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkHeadScript : MonoBehaviour
+{
+    public GameObject[] targets;
+    private int nextTarget = 0;
+    public MinigameProgressTracker MGPT;
+    public GameObject DragMarker;
+    public Material replaceMentMaterial;
+    public GameObject water;
+    public GameObject waterGraphic;
+
+    public void Start()
+    {
+        water.transform.position = targets[0].transform.position;
+        DragMarker.transform.position = targets[0].transform.position;
+    }
+
+    public void Update()
+    {
+        water.transform.position = Vector3.Lerp(water.transform.position, DragMarker.transform.position, 0.05f);
+        waterGraphic.transform.localPosition = 
+            new Vector3(Mathf.Cos(Time.time)*0.1f, 
+                       (Mathf.Cos(Time.time*0.5f)*0.03f)-0.5f,
+                        0);
+    }
+
+
+    public void TargetClicked(int targetID)
+    {
+        if (nextTarget == targetID)
+        {
+            
+            nextTarget++;
+            MGPT.points++;
+            targets[targetID].GetComponent<Renderer>().material = replaceMentMaterial;
+            if (targetID < targets.Length-1)
+            {
+                DragMarker.transform.position = targets[targetID + 1].transform.position;
+            }
+        }
+    }
+}
---
+++ Assets/ParkHeadTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkHeadTarget : MonoBehaviour
+{
+    public ParkHeadScript PHS;
+    public int ID;
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            PHS.TargetClicked(ID);
+        }
+    }
+}
