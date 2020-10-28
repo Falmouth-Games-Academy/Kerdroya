39f7eae3b3b85661ca17943b33f1af82731dcfdf
John-Speakman <45663006+John-Speakman@users.noreply.github.com>
2020-10-28 08:13:41 +0000

    GPS update
    GPS now fades in a pop up if within reach of a target, which shows a UI button which takes the user to the associated scene.

--- Assets/TestLocationService.cs
+++ Assets/TestLocationService.cs
@@ -1,12 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
+using System.Linq;
+using UnityEngine.UIElements;
+
+[System.Serializable]
 
 public class TestLocationService : MonoBehaviour
 {
     public Text outputText;
+    public float minSceneSwapDistance = 1f;
+    public Vector3[] locationData;
+    public GameObject popUpPanel;
+    public UnityEngine.UI.Button sceneSelectBtn;
+    public Text sceneText;
+    private RectTransform RT;
+    public bool animatingPanel = false;
+    public float alphaTranitionSpeed = 0.05f;
+    public float alphaDelta = 0f;
+
     IEnumerator Start()
     {
+        RT = popUpPanel.GetComponent<RectTransform>();
         outputText.text = "Booting...\n";
 
         // First, check if user has location service enabled
@@ -44,7 +60,6 @@ public class TestLocationService : MonoBehaviour
         }
 
         
-
         bool waitingIcon = false;
         while (true)
         {
@@ -53,7 +68,7 @@ public class TestLocationService : MonoBehaviour
             {
                 outputText.color = Color.red;
                 outputText.text = "Connection Status: " + Input.location.status.ToString();
-                break;
+               // break;
             }
             waitingIcon = !waitingIcon;
             string elipseSwap = waitingIcon ? "" : "...";
@@ -63,10 +78,113 @@ public class TestLocationService : MonoBehaviour
                "\nHorizontal Accuracy: " + Input.location.lastData.horizontalAccuracy +
                "\ntimeStamp: " + Input.location.lastData.timestamp +
                "\n" + elipseSwap;
+
+
+            float closestDistance = 0;
+            int bestID = -1;
+            ArrayProximityTest(locationData,
+                                   new Vector3(Input.location.lastData.latitude,
+                                               Input.location.lastData.longitude,
+                                               Input.location.lastData.altitude),
+                                   ref bestID,
+                                   ref closestDistance);
+            SceneSelection(bestID, closestDistance);
             yield return new WaitForSeconds(0.5f);
         }
-
         // Stop service if there is no need to query location updates continuously
         Input.location.Stop();
+    }
+
+    public void ArrayProximityTest(Vector3[] targetLocations, Vector3 comparitor, ref int bestID, ref float closestDistance)
+    {
+        closestDistance = Mathf.Infinity;
+        bestID = -1;
+        int currentID = 0;
+        foreach(Vector3 target in targetLocations)
+        {
+            float distance = Vector3.Distance(target, comparitor);
+            if (closestDistance > distance)
+            {
+                closestDistance = distance;
+                bestID = currentID;
+            }
+            currentID++;
+        }
+        
+    }
+
+    public void SceneSelection(int sceneID, float distance)
+    {
+
+        //check if already loading scene
+        //case for player rejects scene change
+
+        if(distance < minSceneSwapDistance)
+        {
+            animatingPanel = true;
+            switch (sceneID)
+            {
+                case 0 :
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(1));
+                    sceneText.text = "Predennak";
+                    break;
+                case 1 :
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(2));
+                    sceneText.text = "Lansallos";
+                    break;
+                case 2 :
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(3));
+                    sceneText.text = "Tehidy";
+                    break;
+                case 3 :
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(4));
+                    sceneText.text = "Colliford Lake";
+                    break;
+                case 4:
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(5));
+                    sceneText.text = "Boscastle";
+                    break;
+                case 5:
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(6));
+                    sceneText.text = "Morwenstow";
+                    break;
+                case 6:
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(7));
+                    sceneText.text = "Padstow";
+                    break;
+                case 7:
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(10));
+                    sceneText.text = "Cape Cornwall";
+                    break;
+                case 8:
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(11));
+                    sceneText.text = "Clarrick woods";
+                    break;
+                case 9:
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(12));
+                    sceneText.text = "St Agnes Head";
+                    break;
+                case 10:
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(13));
+                    sceneText.text = "The Blouth";
+                    break;
+                case 11:
+                    sceneSelectBtn.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(14));
+                    sceneText.text = "Park Head";
+                    break;
+            }
+
+        }
+    }
+
+    public void FixedUpdate()
+    {
+
+        if (animatingPanel)
+        {
+            alphaDelta += alphaTranitionSpeed;
+            alphaDelta = alphaDelta < 0.95f ? alphaDelta : 1f;
+            sceneSelectBtn.GetComponent<UnityEngine.UI.Image>().color = new Color(1, 1, 1, alphaDelta);
+        }
     }
 }
