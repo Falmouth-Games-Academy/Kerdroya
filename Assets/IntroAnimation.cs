404da370f15191137ab2d3e524bef73e1c796b92
Tim <timphillipsgames@gmail.com>
2021-01-17 14:11:54 +0000

    Changed font of text in intro

--- Assets/IntroAnimation.cs
+++ Assets/IntroAnimation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class IntroAnimation : MonoBehaviour
 {
@@ -259,7 +260,7 @@ public class IntroAnimation : MonoBehaviour
 
                 break;
             case (IntroStates)22:
-                //Debug.Log("intro ends");
+                SceneManager.LoadScene("MainMenu");
 
                 break;
 
