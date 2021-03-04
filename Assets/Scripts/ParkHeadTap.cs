c984df1e8ee38017861873e134f47d094abad4d4
Tim <timphillipsgames@gmail.com>
2021-03-04 17:26:41 +0000

    Park Head has new clearer art

---
+++ Assets/Scripts/ParkHeadTap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider))]
+
+public class ParkHeadTap : MonoBehaviour, IPointerClickHandler
+{
+
+    public MinigameProgressTracker miniGameProgressTracker;
+
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        if (GetComponent<SpriteRenderer>().enabled == false)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            miniGameProgressTracker.points++;
+        }
+        
+    }
+}
