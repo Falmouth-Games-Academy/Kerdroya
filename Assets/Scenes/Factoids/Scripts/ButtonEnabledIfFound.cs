18226a5a04d99593549e7c8bea231843cd86d7e2
Alcwyn Parker <alcwyn.parker@falmouth.ac.uk>
2021-04-01 13:56:54 +0100

    Factoid Menu reactive to the found items

---
+++ Assets/Scenes/Factoids/Scripts/ButtonEnabledIfFound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEnabledIfFound : MonoBehaviour
+{
+  public int levelIndex = 0;
+  Button mainButton;
+  void Start()
+  {
+    mainButton = GetComponent<Button>();
+    if (AppProgression.levelCompleted[levelIndex] == true) mainButton.interactable = true;
+  }
+}
---
+++ Assets/Scenes/Factoids/Scripts/EyeUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeUI : MonoBehaviour
+{
+    public int levelIndex = 0;
+    public bool desiredState = true;
+    void Start()
+    {
+        if (AppProgression.levelCompleted[levelIndex] != desiredState) gameObject.SetActive(false);
+    }
+
+}
