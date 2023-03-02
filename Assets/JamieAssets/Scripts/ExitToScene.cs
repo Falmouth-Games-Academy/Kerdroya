6e1daf71f0583b02adb9b72808e1cd346812ca66
JamieCDev20 <JamieCDevelopment@gmail.com>
2023-03-02 14:27:55 +0000

    Added non-functional back button to all scenes

---
+++ Assets/JamieAssets/Scripts/ExitToScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitToScene : MonoBehaviour
+{
+    [SerializeField] private int scene;
+
+
+    public void OnClick()
+    {
+        print("Should load scene " + Time.realtimeSinceStartup);
+        SceneManager.LoadScene(scene);
+    }
+
+}
